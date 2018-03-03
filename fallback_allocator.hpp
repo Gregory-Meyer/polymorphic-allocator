07dd13dbaa36b3d054738ecce13e2ff691284bba
Gregory Meyer <gregjm@umich.edu>
2018-03-03 11:45:44 -0500

    more noexcept, pair for piecewise construction, deleted copy operations

--- fallback_allocator.hpp
+++ fallback_allocator.hpp
@@ -6,22 +6,38 @@
                                      // gregjm::BadAllocationException
 
 #include <algorithm> // std::max
-#include <type_traits>
-#include <utility> // std::move
+#include <type_traits> // std::is_nothrow_constructible_v,
+                       // std::is_constructible_v, std::enable_if_t
+#include <utility> // std::move, std::pair, std::piecewise_construct
+#include <tuple> // std::tuple
 
 namespace gregjm {
 
 template <typename Primary, typename Secondary>
 class FallbackAllocator : public PolymorphicAllocator {
+    using PairT = std::pair<Primary, Secondary>;
 public:
     FallbackAllocator() = default;
 
+    FallbackAllocator(const FallbackAllocator &other) = delete;
+
     FallbackAllocator(Primary primary, Secondary secondary)
-        noexcept(std::is_nothrow_move_constructible_v<Primary>
-                 and std::is_nothrow_move_constructible_v<Secondary>)
-        : primary_{ std::move(primary) },
-          secondary_{ std::move(secondary) }
+        noexcept(
+            std::is_nothrow_constructible_v<PairT, Primary&&, Secondary&&>
+        ) : allocs_{ std::move(primary), std::move(secondary) }
     { }
+
+    template <typename ...PrimaryArgs, typename ...SecondaryArgs, typename = std::enable_if_t<std::is_constructible_v<Primary, PrimaryArgs...> and std::is_constructible_v<Secondary, SecondaryArgs...>>>
+    FallbackAllocator(const std::tuple<PrimaryArgs...> primary_args,
+                      const std::tuple<SecondaryArgs...> secondary_args)
+        noexcept(
+            std::is_nothrow_constructible_v<PairT, std::piecewise_construct_t,
+                                            std::tuple<PrimaryArgs...>,
+                                            std::tuple<SecondaryArgs...>>
+        ) : allocs_{ std::piecewise_construct, primary_args, secondary_args }
+    { }
+
+    FallbackAllocator& operator=(const FallbackAllocator &other) = delete;
 
 private:
     MemoryBlock allocate_impl(const std::size_t count,
@@ -29,42 +45,56 @@ private:
         MemoryBlock block;
 
         try {
-            block = primary_.allocate(count, alignment);
+            block = primary().allocate(count, alignment);
         } catch (const BadAllocationException &e) {
             // if this throws, we would just rethrow so no need to try-catch
-            block = secondary_.allocate(count, alignment);
+            block = secondary().allocate(count, alignment);
         }
 
         return block;
     }
 
     void deallocate_impl(const MemoryBlock block) override {
-        if (primary_.owns(block)) {
-            primary_.deallocate(block);
-        } else if (secondary_.owns(block)) {
-            secondary_.deallocate(block);
+        if (primary().owns(block)) {
+            primary().deallocate(block);
+        } else if (secondary().owns(block)) {
+            secondary().deallocate(block);
         } else {
             throw NotOwnedException{ };
         }
     }
 
     void deallocate_all_impl() override {
-        primary_.deallocate_all();
-        secondary_.deallocate_all();
+        primary().deallocate_all();
+        secondary().deallocate_all();
     }
 
     std::size_t max_size_impl() const override {
-        return std::max(primary_.max_size(),
-                        secondary_.max_size());
+        return std::max(primary().max_size(),
+                        secondary().max_size());
     }
 
     bool owns_impl(const MemoryBlock block) const override {
-        return primary_.owns(block)
-               or secondary_.owns(block);
+        return primary().owns(block) or secondary().owns(block);
     }
 
-    Primary primary_;
-    Secondary secondary_;
+    Primary& primary() noexcept {
+        return allocs_.first;
+    }
+
+    const Primary& primary() const noexcept {
+        return allocs_.first;
+    }
+
+    Secondary& secondary() noexcept {
+        return allocs_.second;
+    }
+
+    const Secondary& secondary() const noexcept {
+        return allocs_.second;
+    }
+
+    PairT allocs_;
 };
 
 } // namespace gregjm
