622c8296cdb8ea111b6830f4f06c50defc15b7e9
Gregory Meyer <gregjm@umich.edu>
2018-03-03 12:25:39 -0500

    more inline, added more dummy locking functions
    Signed-off-by: Gregory Meyer <gregjm@umich.edu>

--- dummy_mutex.hpp
+++ dummy_mutex.hpp
@@ -1,13 +1,33 @@
 #ifndef GREGJM_DUMMY_MUTEX_HPP
 #define GREGJM_DUMMY_MUTEX_HPP
 
+#include <chrono>
+
 namespace gregjm {
 
 class DummyMutex {
 public:
-    constexpr void lock() const noexcept { }
+    constexpr inline void lock() const noexcept { }
 
-    constexpr void unlock() const noexcept { } 
+    constexpr inline bool try_lock() const noexcept {
+        return true;
+    }
+
+    template <typename Rep, typename Period>
+    constexpr inline bool try_lock_for(
+        const std::chrono::duration<Rep, Period>&
+    ) const noexcept {
+        return true;
+    }
+
+    template <typename Clock, typename Duration>
+    constexpr inline bool try_lock_until(
+        const std::chrono::time_point<Clock, Duration>&
+    ) const noexcept {
+        return true;
+    }
+
+    constexpr inline void unlock() const noexcept { } 
 };
 
 } // namespace gregjm
